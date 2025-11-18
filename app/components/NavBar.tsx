8772e6afa5f204a854efb39fafba3f06e26e860f
Sir-josh <docsmile01@gmail.com>
2025-11-18 15:54:36 +0100

    modified the navBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,3 @@
-// import React from 'react'
-
-// export default function NavBar() {
-//   return (
-//     <nav className='flex justify-between m-2 border-1'>
-//       <div>logo</div>
-//       <div><i>Icon</i>Menu</div>
-//     </nav>
-//   )
-// }
-// src/app/components/NavBar.jsx
 
 "use client";
 import React, { useState } from "react";
